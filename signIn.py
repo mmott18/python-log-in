89933445702f94b5cf0eada1edf06f5649a5018e
Matt <mmott@udel.edu>
2018-05-14 02:18:31 -0400

    countdown timer if user enters incorrect credentials too many times

--- signIn.py
+++ signIn.py
@@ -1,5 +1,15 @@
 from sqlite import *
 from security import *
+import time
+
+
+def countdown(t):
+    print("Too many incorrect sign in attempts.")
+    print("Please wait for " + str(t) + " seconds.")
+    while t:
+        print(t)
+        time.sleep(1)
+        t -= 1
 
 
 def register_user():
@@ -15,19 +25,30 @@ def register_user():
 
 
 def sign_in_user():
-    username = input("Please enter a username: ")
-    password = input("Please enter a password: ")
+    attempts = 1
+    success = False
 
-    hashed_user = hash_data(username)
-    hashed_pass = hash_data(password)
+    while not success:
+        username = input("Please enter a username: ")
+        password = input("Please enter a password: ")
 
-    if check_user(hashed_user) == 0:
-        print("Incorrect username or password")
-    else:
-        if check_pass(hashed_pass) == 0:
-            print("Incorrect username or password.")
+        hashed_user = hash_data(username)
+        hashed_pass = hash_data(password)
+
+        if attempts > 2: #FIXME if user takes 3 or more attempts to enter correct PW, then enters it correctly, still enters this
+                         #FIXME because attempts is still > 2
+            diff = attempts - 2
+            print(countdown(3 ** diff))
+        if check_user(hashed_user) == 0:
+            print("Incorrect username or password")
         else:
-            print("Welcome, " + username + ".")
+            if check_pass(hashed_pass) == 0:
+                print("Incorrect username or password.")
+                attempts += 1
+            else:
+                attempts = 0
+                print("Welcome, " + username + ".")
+                success = True
 
 
 def menu():
